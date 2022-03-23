3aef8b0d61ffc7b0c07ea9763cfcc5d96e068c74
alejandrofdez-us <afdez@us.es>
2022-03-23 18:59:48 +0100

    removed unused imports

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import AppContextProvider from './src/context/AppContext'
-import AuthorizationContextProvider, { AuthorizationContext } from './src/context/AuthorizationContext'
+import AuthorizationContextProvider from './src/context/AuthorizationContext'
 import Layout from './src/screens/Layout'
 
 export default function App () {
